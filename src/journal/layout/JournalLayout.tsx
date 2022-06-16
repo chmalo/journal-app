df146baa9623da48dd8efe9ae37581e5ce84d504
Chmalo <chmalo.f@gmail.com>
2022-06-16 09:24:06 -0400

    feat navbar

---
+++ src/journal/layout/JournalLayout.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material"
+import { NavBar } from "../components"
+
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }: { children: any }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">Journal page</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Typography>
+    </JournalLayout>
   )
 }
